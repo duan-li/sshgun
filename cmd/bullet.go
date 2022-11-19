afb83e71ee7bf88bdedfb30e83b2d04536f746c4
Duan L <inputx@gmail.com>
2022-11-20 10:37:53 +1100

    add command validate yaml file

--- cmd/bullet.go
+++ cmd/bullet.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	bullet "github.com/duan-li/sshgun/bullet"
 	"github.com/urfave/cli/v2"
 	"log"
 	"os"
@@ -41,14 +42,33 @@ func InitBullet() func(cCtx *cli.Context) error {
 		if err1 != nil {
 			log.Fatal(err1)
 		}
-		fmt.Println("new template created: ", cCtx.Args().First())
-		return nil
+		return cli.Exit("New template created: "+file, 0)
 	}
 }
 
 func Validate() func(cCtx *cli.Context) error {
 	return func(cCtx *cli.Context) error {
-		fmt.Println("validate template: ", cCtx.Args().First())
-		return nil
+		if cCtx.NArg() < 1 {
+			return fmt.Errorf("Missing file name.")
+		}
+		file := cCtx.Args().First()
+
+		dir, err := os.Getwd()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		yfile, err := os.ReadFile(dir + "/" + file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err1 := bullet.Paser(yfile)
+
+		if err1 != nil {
+			log.Fatal(err1)
+		}
+
+		return cli.Exit("Template is valid: "+file, 0)
 	}
 }
