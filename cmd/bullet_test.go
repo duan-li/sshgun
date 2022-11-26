14047e6c3a5eeac99c2156a2496174ed5aeb470a
Duan L <inputx@gmail.com>
2022-11-26 16:17:59 +1100

    test case for command

---
+++ cmd/bullet_test.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestInitBullet(t *testing.T) {
+	f := InitBullet()
+	assert.NotNil(t, f)
+}
+
+func TestValidate(t *testing.T) {
+	f := Validate()
+	assert.NotNil(t, f)
+}
+
+func TestRun(t *testing.T) {
+	f := Run()
+	assert.NotNil(t, f)
+}
+
+func TestCheck(t *testing.T) {
+	f := Check()
+	assert.NotNil(t, f)
+}
