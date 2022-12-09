e7893436834937fada237909f4bd0a8db4d9083a
Duan L <inputx@gmail.com>
2022-12-09 22:05:45 +1100

    add docs

--- ssh/config.go
+++ ssh/config.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 )
 
+// Config is the configuration struct for the SSH connection
 type Config struct {
 	Host       string
 	Port       int
@@ -13,10 +14,12 @@ type Config struct {
 	SuPassword string
 }
 
+// HostAddress returns the host address
 func HostAddress(config Config) string {
 	return config.Host + ":" + strconv.Itoa(config.Port)
 }
 
+// NewConfig returns a new SSH configuration
 func NewConfig(host string, port int, user string, pass string, supass string) Config {
 	return Config{
 		Host:       host,
@@ -27,6 +30,7 @@ func NewConfig(host string, port int, user string, pass string, supass string) C
 	}
 }
 
+// NewSSHConfig create new SSH configuration
 func NewSSHConfig(config Config) *ssh.ClientConfig {
 	sshConfig := &ssh.ClientConfig{
 		User: config.User,
